Python3 update (via 2to3 plus some by hand) Also fix for matplotlib (3?) not masking negative points by default in log plots

realization/ng.py
@@ -12,9 +12,9 @@ smaller than the box).
 
 ### DONE: add excising a region to local_fNL:
 
-from __future__ import division
-from __future__ import with_statement
-from __future__ import print_function
+
+
+
 
 import math
 import numbers
@@ -24,8 +24,8 @@ import numpy.random as Nr
 
 import scipy.stats as sps
 
-import realization
-import aniso
+from . import realization
+from . import aniso
 
 def local_fNL(dims, fNL, Pk=None, deltas=None, ng_pow=2, return_config=False,
               fG=1.0, transform=None, excise=None):
@@ -123,7 +123,7 @@ def get_fourier_dist(rlzn, deltas=None, nk=10, nbins=30,
             ### or average over the actual k that contribute?
 
         rlzn_bin = rlzn[kdxi]
-        full_bin = np.hstack((rlzn_bin.real,rlzn_bin.imag)).ravel()
+        full_bin = np.hstack((rlzn_bin.real, rlzn_bin.imag)).ravel()
         if normalized:
             full_bin = (full_bin - full_bin.mean())/full_bin.std()
 
@@ -137,7 +137,7 @@ def get_fourier_dist(rlzn, deltas=None, nk=10, nbins=30,
     return np.array(kout), np.array(stats), hists
 
 
-def driver(dims = (256,256,256),
+def driver(dims = (256, 256, 256),
             fNL = 1000.0,
             deltas = None,
             nk = 10,

realization/pixel_sample.py
@@ -19,7 +19,7 @@ DONE: check mode-by-mode (kx, ky) pixel windows
 ### nb. 
 
 
-from __future__ import print_function
+
 
 import math
 from distutils.version import LooseVersion
@@ -31,21 +31,16 @@ import scipy.special as sps
 
 import scipy.ndimage.filters as snf
 
-import realization
-import aniso
+from . import realization
+from . import aniso
 import itertools
 
-try:
-    from itertools import izip
-except ImportError:
-    izip = zip
-    
     
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
     a, b = itertools.tee(iterable)
     next(b, None)
-    return izip(a, b)
+    return zip(a, b)
     
 
 def pixelWindowCircle(nu, L):
@@ -128,7 +123,7 @@ def pixelSample(rlzn, nsamples=None, shrink_by=2):
     print("len=%d" % rlzn.size)
     
     ## check that the the new shape is comensurate with the old shape
-    assert all(s_out*shrink_by==s_in for s_in,s_out in zip(shape_in, shape_out))
+    assert all(s_out*shrink_by==s_in for s_in, s_out in zip(shape_in, shape_out))
     
     ### could use np.random.choice on the flattened array, 
     ###  but it's easier to track the actual array indices
@@ -136,7 +131,7 @@ def pixelSample(rlzn, nsamples=None, shrink_by=2):
     vals = rlzn.ravel()[idxs_1d]
     print("vals[%d]: %g ± %g" % (vals.size, vals.mean(), vals.std()))
 
-    idxs_nd = np.unravel_index(idxs_1d,shape_in)
+    idxs_nd = np.unravel_index(idxs_1d, shape_in)
     
     idxs_nd_sampled = (np.array(idxs_nd, dtype=np.int)//shrink_by).transpose()
     rlzn_sampled = np.zeros(shape_out)
@@ -148,7 +143,7 @@ def pixelSample(rlzn, nsamples=None, shrink_by=2):
         
     idxs_good = (n_in_pix>0)
     print("final shape:", idxs_good.shape)
-    print("good/total: %d/%d" % (idxs_good.sum(),idxs_good.size))
+    print("good/total: %d/%d" % (idxs_good.sum(), idxs_good.size))
     
     rlzn_sampled[idxs_good] /= n_in_pix[idxs_good] 
     print("orig: %g ± %g" % (rlzn.mean(), rlzn.std()))
@@ -224,21 +219,21 @@ def subsamplePower(Pk_fun, rshape, shrink_by=2, deltas=None, nk=10,
         kk = np.linspace(0, k.max(), nk)
     kout = np.zeros_like(kk)
     aa = np.linspace(0, 2*np.pi, nangle+1)
-    Pk = np.zeros(shape=(nk,nangle), dtype=np.float64)
+    Pk = np.zeros(shape=(nk, nangle), dtype=np.float64)
 
 ### iterating over all combinations of the -n...+n range
 ### see http://stackoverflow.com/questions/1208118/using-numpy-to-build-an-array-of-all-combinations-of-two-arrays
     ### nmaxs should really depend on where you are, such that the ktot<kmax
     ### therefore should really be inside the innermost loop below (see abs(ktot) check)
     nmaxs = (nmax,)*ndims  ### can be different in each direction
-    rr = np.array([range(-nm,nm+1) for nm in nmaxs])
+    rr = np.array([list(range(-nm, nm+1)) for nm in nmaxs])
     kiter = np.array(np.meshgrid(*rr), dtype=np.float).T.reshape(-1, ndims)
     rfac = np.array(shrink_by, dtype=np.float)*np.array(deltas, dtype=np.float)
     kiter /= rfac   ## rescaling from raw pixels to the samples
 #     print("rfac=", rfac)
 #     print("kiter=", kiter)
     
-    for ii,ki in enumerate(pairwise(kk)):   ### need to iterate over full k_vec in the bin as well.
+    for ii, ki in enumerate(pairwise(kk)):   ### need to iterate over full k_vec in the bin as well.
         kdxi = np.logical_and(k>ki[0], k<=ki[1])
         for jj, aj in enumerate(pairwise(aa)):
             if nangle>1:
@@ -248,7 +243,7 @@ def subsamplePower(Pk_fun, rshape, shrink_by=2, deltas=None, nk=10,
                 idx = kdxi
                 
             ## here, idx has list pointing into power, or k
-            kvec_bin = kvec[:,idx].transpose()
+            kvec_bin = kvec[:, idx].transpose()
             powk = np.zeros(idx.sum())  ### hold the aliased power
 
             for ip, kv in enumerate(kvec_bin):
@@ -281,6 +276,9 @@ def subsamplePower(Pk_fun, rshape, shrink_by=2, deltas=None, nk=10,
     if nangle > 1: 
         return (kout), Pk
     else:
+        ### AHJ remove P(k)=0 points. Is this really what we want to do?
+        #### No? Fix by adding "nonposy='mask' in plot.
+ #       return kout[Pk>0], Pk[Pk>0]
         return kout, Pk
 
 
@@ -295,7 +293,7 @@ def samplingNoise():
     """
 
     
-def driver(n=0, dims=(512,512), deltas=1, nsamples=1e6, shrink_by=2, 
+def driver(n=0, dims=(512, 512), deltas=1, nsamples=1e6, shrink_by=2, 
            nk=20,allplots=False, eq_vol=False, maps=True, plot_ratio=True,
            cutraw=2, title=""):
 
@@ -341,7 +339,7 @@ def driver(n=0, dims=(512,512), deltas=1, nsamples=1e6, shrink_by=2,
                                 eq_vol=eq_vol)
     eP *= fac; eS *= fac
 
-    print('P[0]/eP[0]=%g, shrink**4=%g' % (P[0]/eP[0],fac))
+    print('P[0]/eP[0]=%g, shrink**4=%g' % (P[0]/eP[0], fac))
     
     ### mode-by-mode correction for pixel window:
     sampled_delta_r_unwindowed = unWindow(sampled_delta_r, deltas=deltas*shrink_by)
@@ -355,7 +353,9 @@ def driver(n=0, dims=(512,512), deltas=1, nsamples=1e6, shrink_by=2,
     ak, aP, aS = aniso.getPower(avg_delta_r, nk=nk*shrink_by, deltas=deltas, eq_vol=eq_vol)
     
     ### running average, subsampled
-    sampled_avg_delta_r = avg_delta_r[[slice(None, None, shrink_by)]*ndims]
+    ### AHJ "tuple" fixes a deprecation warning of the differences between arrays and tuples
+    ### as indexes
+    sampled_avg_delta_r = avg_delta_r[tuple([slice(None, None, shrink_by)]*ndims)]
 
     eak, eaP, eaS = aniso.getPower(sampled_avg_delta_r, nk=nk, deltas=deltas*shrink_by, 
                                 eq_vol=eq_vol)
@@ -386,7 +386,7 @@ def driver(n=0, dims=(512,512), deltas=1, nsamples=1e6, shrink_by=2,
     if pixelWindow:
     
         ### sampled n_out pixels
-        pixwin2 = pixelWindow(ek,deltas*shrink_by)**2  ### only work for scalar deltas
+        pixwin2 = pixelWindow(ek, deltas*shrink_by)**2  ### only work for scalar deltas
         #pixwin2 /= pixelWindow(ek,deltas)**2 ### divide by single-pixel window (doesn't matter?)
         eP_unwin= eP/pixwin2
         eS_unwin= eS/pixwin2
@@ -395,7 +395,7 @@ def driver(n=0, dims=(512,512), deltas=1, nsamples=1e6, shrink_by=2,
         eaS_unwin= eaS/pixwin2
         
         ### full n_in pixels
-        pixwin2a = pixelWindow(ak,deltas*shrink_by)**2  ### only work for scalar deltas
+        pixwin2a = pixelWindow(ak, deltas*shrink_by)**2  ### only work for scalar deltas
         #pixwin2a /= pixelWindow(ak,deltas)**2 ### divide by single-pixel window (doesn't matter?)
         aP_unwin= aP/pixwin2a
         aS_unwin= aS/pixwin2a
@@ -437,7 +437,7 @@ def driver(n=0, dims=(512,512), deltas=1, nsamples=1e6, shrink_by=2,
         
 #       plt.loglog(ek[1:], eP_unwin[1:]+eS_unwin[1:])
     if plot_aliased_theory:
-        plt.loglog(k_aliased[1:], P_aliased[1:], marker=None, color=colors[8], label='aliased theory')
+        plt.loglog(k_aliased[1:], P_aliased[1:], marker=None, color=colors[8], label='aliased theory', nonposy='mask')
         
     plt.legend(loc='best', frameon=False)
     plt.xlim(ek[1:].min(), ek[1:].max())
@@ -449,7 +449,7 @@ def driver(n=0, dims=(512,512), deltas=1, nsamples=1e6, shrink_by=2,
     if ndims==2:
         if not cutraw:
             plt.axvline(k.max()/math.sqrt(2), ls='dotted', c='grey')
-        plt.axvline(ek.max()/math.sqrt(2),ls='dotted', c='grey')
+        plt.axvline(ek.max()/math.sqrt(2), ls='dotted', c='grey')
 
     
     if plot_ratio:
@@ -468,10 +468,11 @@ def driver(n=0, dims=(512,512), deltas=1, nsamples=1e6, shrink_by=2,
             rplotter(eak[1:], eaP_unwin[1:]/Pk_theory[:len(ek)-1], 
                         label="sampled, avg unwin/theory", marker=None, color=colors[7])
         if plot_aliased_theory:
-            rplotter(k_aliased[1:], P_aliased[1:]/Pk(2*np.pi*k_aliased[1:]), label='aliased/theory', color=colors[8], marker=mrkr)
+            rplotter(k_aliased[1:], P_aliased[1:]/Pk(2*np.pi*k_aliased[1:]), label='aliased/theory', 
+                     color=colors[8], marker=mrkr, nonposy='mask')
         plt.xlim(ek[1:].min(), ek[1:].max())
         plt.ylim(0.5, 5.0)
-        plt.axhline(1.0,ls='dotted', c='grey')
+        plt.axhline(1.0, ls='dotted', c='grey')
         plt.legend(loc='best', frameon=False)
         plt.title(title)
 
@@ -486,14 +487,14 @@ def driver(n=0, dims=(512,512), deltas=1, nsamples=1e6, shrink_by=2,
         nc = int(nrc/nr)
         if nr*nc<nrc: nc += 1
 
-        for i in xrange(1,nk):
+        for i in range(1, nk):
             plt.subplot(nr, nc, i)
             ang, rlzk, k = aniso.getAngDist(sampled_delta_r, i, nk=nktot)
             std = np.sqrt(((np.absolute(rlzk))**2).mean())
             plt.plot(ang, rlzk.real, '.')
             plt.plot(ang, rlzk.imag, '.')
-            plt.plot([0,math.pi], [std,std], 'r')
-            plt.plot([0,math.pi], [-std,-std], 'r')
+            plt.plot([0, math.pi], [std, std], 'r')
+            plt.plot([0, math.pi], [-std, -std], 'r')
             ax = plt.gca()
             lab = r'$k=%f$' % k
             plt.text(0.1, 0.9, lab, transform = ax.transAxes)
@@ -509,7 +510,7 @@ def driver(n=0, dims=(512,512), deltas=1, nsamples=1e6, shrink_by=2,
         plt.figure()
         lentot = len(delta_r)
         plt.plot(np.arange(lentot), delta_r, label=r'raw $\delta({\bf r})$')
-        plt.plot(np.arange(0,lentot,shrink_by), sampled_delta_r, label='sampled')
+        plt.plot(np.arange(0, lentot, shrink_by), sampled_delta_r, label='sampled')
         plt.legend(loc='best', frameon=False)
         plt.title(r'$\delta({\bf r}$)')
 

realization/realization.py
@@ -9,8 +9,8 @@ arbitrary number of dimensions and number of points in each
 #### maybe make a class that carries along delta with it? (see velocities.Field)
 #### 
 
-from __future__ import division
-from __future__ import with_statement
+
+
 
 import math
 import numbers
@@ -97,7 +97,7 @@ def DFT_indices(dimensions, dtype=np.int_, dim1=None, real=False):
     lst = []
     
     if dim1 is None:
-        dim1 = range(ndims)
+        dim1 = list(range(ndims))
     
     for i in dim1:
         d = dims[i]

realization/velocities.py
@@ -18,8 +18,8 @@ Created by Andrew H. Jaffe on 2009-12-11.
 Copyright (c) 2009 Imperial College London. All rights reserved.
 """
 
-from __future__ import division
-from __future__ import with_statement
+
+
 
 import math
 import sys
@@ -37,13 +37,13 @@ try:
     import matplotlib.pyplot as plt
 except ImportError:
     canPlot = False
-    print "can't plot"
+    print("can't plot")
 
 import numpy.random as rnd
 
-import realization as rlzn
+from . import realization as rlzn
 
-import power
+from . import power
 
 ### NB fourier conventions
 ## rfftn: F_k = \sum_n f_n exp(-2pi ikn/N)
@@ -244,7 +244,7 @@ class FieldWithVelocities(Field):
         return self.deltas*idxs, vels
 
 
-def processCatalog(posns, vels, verr=0, nonlin=0, ctr=(0,0,0), lens=(1,1,1)):
+def processCatalog(posns, vels, verr=0, nonlin=0, ctr=(0, 0, 0), lens=(1, 1, 1)):
     """
        re-center catalog at an arbitrary point in the volume
        convert n-d velocity catalog to radial velocities. 
@@ -279,7 +279,7 @@ def plotcatalog(cat):
     
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(cat[:,0], cat[:,1], cat[:,2])
+    ax.scatter(cat[:, 0], cat[:, 1], cat[:, 2])
     plt.show()
     
     
@@ -287,12 +287,12 @@ def toPolar(pos, deg=True):
     """ convert a shape=(:, 3) cartesian coordinate array to polars """
     r = np.sqrt((pos**2).sum(axis=-1))
     if deg:
-        theta = 90-np.degrees(np.arccos(pos[:,2]/r))
-        phi = np.degrees(np.arctan2(pos[:,1], pos[:,0]))
+        theta = 90-np.degrees(np.arccos(pos[:, 2]/r))
+        phi = np.degrees(np.arctan2(pos[:, 1], pos[:, 0]))
     else:
-        theta = np.pi/2-np.arccos(pos[:,2]/r)
-        phi = np.arctan2(pos[:,1], pos[:,0])
-    return np.vstack((r,theta,phi)).transpose()
+        theta = np.pi/2-np.arccos(pos[:, 2]/r)
+        phi = np.arctan2(pos[:, 1], pos[:, 0])
+    return np.vstack((r, theta, phi)).transpose()
     
     
 def generate_velocity_catalog(N=256, nbar=500, length=100, verr=1000, nonlin=250,
@@ -316,11 +316,11 @@ def generate_velocity_catalog(N=256, nbar=500, length=100, verr=1000, nonlin=250
     
     powspec = power.PowSpec(power.bbks, smooth=0)
     
-    fk = rlzn.dft_realizn(shape,Pk=powspec, deltas=deltas) 
+    fk = rlzn.dft_realizn(shape, Pk=powspec, deltas=deltas) 
     
     dipole2 = power.sigma2_R(powspec, R=length/2.0, kpow=-2, prefac=(a0H0*fgrav)**2)
     dipole_rms = math.sqrt(dipole2)     ## REALLY SHOULD BE FULL BOX, NOT SPHERE
-    print "#dipole v_rms = %f" % dipole_rms
+    print("#dipole v_rms = %f" % dipole_rms)
 
     field = FieldWithVelocities(delta_k=fk, deltas=deltas, dipole_rms=dipole_rms)
     
@@ -339,15 +339,15 @@ def generate_velocity_catalog(N=256, nbar=500, length=100, verr=1000, nonlin=250
         polar = toPolar(pos)
     
         if stdout:
-            print ("#%5s %8s %8s %8s %8s %8s %8s %8s" %
-                   ("i", "r", "theta", "phi" ,"v_x","v_y","v_z", "S_r"))
+            print(("#%5s %8s %8s %8s %8s %8s %8s %8s" %
+                   ("i", "r", "theta", "phi", "v_x", "v_y", "v_z", "S_r")))
             for i, xyz_vxyz_vr in enumerate(zip(polar, vel, vr)):
-                (r,t,p), (vx, vy, vz), vr  = xyz_vxyz_vr
+                (r, t, p), (vx, vy, vz), vr  = xyz_vxyz_vr
             
-                print (" %5d %8.2f %8.2f %8.2f %8.2f %8.2f %8.2f %8.2f" %
-                       (i, r,t,p, vx,vy,vz, vr))
+                print((" %5d %8.2f %8.2f %8.2f %8.2f %8.2f %8.2f %8.2f" %
+                       (i, r, t, p, vx, vy, vz, vr)))
     else:
-        print("No output for ndim=%d" % ndim)
+        print(("No output for ndim=%d" % ndim))
 
     if ret:
         return field, pos, vel, vr
@@ -378,7 +378,7 @@ def driver():
     
     for i in range(nrun):
         fname = "SFI_mock_%d.txt" % i
-        print "file ", fname
+        print("file ", fname)
         old_stdout = sys.stdout
         with open(fname, 'w') as sys.stdout:
             main(opts)
@@ -404,8 +404,8 @@ def main(options=None):
                           help="plot with matplotlib")
         (options, args) = parser.parse_args()
     else:
-        print ("# npoints=%d nbar=%d length=%6.1fMpc verr=%6.1fkm/s nonlin=%6.1fkm/s" %
-               (options.npoints, options.nbar, options.length, options.verr, options.nonlin))
+        print(("# npoints=%d nbar=%d length=%6.1fMpc verr=%6.1fkm/s nonlin=%6.1fkm/s" %
+               (options.npoints, options.nbar, options.length, options.verr, options.nonlin)))
 
     generate_velocity_catalog(N=options.npoints, nbar=options.nbar, length=options.length, 
                      verr=options.verr, nonlin=options.nonlin,